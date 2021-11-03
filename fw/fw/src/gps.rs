d0492eebbbf8931cb44036ad0174bba005db49e3
Denis Chaplygin <akashihi@gmail.com>
2021-11-03 11:54:01 +0200

    Added message count based gps timeout

--- fw/fw/src/gps.rs
+++ fw/fw/src/gps.rs
@@ -1,5 +1,6 @@
 use core::borrow::{Borrow, BorrowMut};
 use core::cell::{Cell, RefCell};
+use core::sync::atomic::{AtomicU16, Ordering};
 use cortex_m::interrupt::Mutex;
 use board::{GpsEnPin, GpsUsart};
 use board::hal::serial::{Event, Rx};
@@ -13,6 +14,7 @@ use board::hal::hal::digital::v2::OutputPin;
 
 type NmeaBuffer = heapless::String<84>;
 
+static MESSAGES_SEEN: AtomicU16 = AtomicU16::new(0);
 static GPS_RX: Mutex<RefCell<Option<Rx<USART2>>>> = Mutex::new(RefCell::new(None));
 static RECEIVE_BUFFER: Mutex<RefCell<NmeaBuffer>> = Mutex::new(RefCell::new(heapless::String::new()));
 static EOL_FLAG: Mutex<Cell<bool>> = Mutex::new(Cell::new(false));
@@ -37,7 +39,10 @@ fn USART2() {
                     }
                     let mut buffer = RECEIVE_BUFFER.borrow(cs).borrow_mut();
                     if buffer.len()>83 {
-                        buffer.clear()
+                        buffer.clear();
+                        // Mark too big string as a message, so we don't spend
+                        // too much time reading garbage
+                        MESSAGES_SEEN.fetch_add(1, Ordering::Relaxed);
                     }
                     if let Err(_) = buffer.push(byte as char) {
                         //Unable to proceed, let's clear it
@@ -51,7 +56,8 @@ fn USART2() {
 
 fn parse_nmea_string(nmea: &str) -> Option<()> {
     if let Some(message) = nmea.get(3..6) {
-        if message == "GGA" || message == "RMC" || message == "GSV" {
+        if message == "RMC" {
+            MESSAGES_SEEN.fetch_add(1, Ordering::Relaxed);
             hprintln!("{}", nmea);
         }
     }
@@ -70,16 +76,28 @@ impl Gps {
 
     pub fn sync_date_time(&mut self) {
         loop {
+            if MESSAGES_SEEN.load(Ordering::Relaxed) > 540 { //We should receive one RMC message per second, so after 540 messages(=9 minutes) we time out
+                break;
+            }
             ci::free(|cs| {
                 if EOL_FLAG.borrow(cs).borrow().get() {
                     // Full line is received, parse it
-                    hprintln!("{}", &RECEIVE_BUFFER.borrow(cs).borrow());
-                    //parse_nmea_string(&RECEIVE_BUFFER.borrow(cs).borrow());
+                    parse_nmea_string(&RECEIVE_BUFFER.borrow(cs).borrow());
                     RECEIVE_BUFFER.borrow(cs).borrow_mut().clear();
                     EOL_FLAG.borrow(cs).borrow_mut().set(false);
                 }
             });
             cortex_m::asm::wfi(); //Sleep till next char arrives
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::gps::parse_nmea_string;
+
+    #[test]
+    fn non_rmc_message_skipped() {
+        assert!(parse_nmea_string("$GPGSV,3,1,12,01,15,170,20,02,08,326,18,03,63,126,22,04,66,205,*7D").is_none());
     }
 }
